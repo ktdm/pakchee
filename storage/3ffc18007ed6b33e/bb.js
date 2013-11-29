ee0888e7e90ccf65dbdd96d671ce91abd315c295
ktdm <aidan.hamwood@gmail.com>
2013-11-30 02:30:25 +1000

    Work towards bulletin board gui

--- storage/3ffc18007ed6b33e/bb.js
+++ storage/3ffc18007ed6b33e/bb.js
@@ -1,5 +1,11 @@
 function $ (id) { return document.getElementById(id) }
 
+function addEvent (el, event, fn) {
+ el.addEventListener ?
+  el.addEventListener(event, fn, false) :
+  el.attachEvent("on" + event, function () { return fn.call(el, window.event) })
+}
+
 function init () {
  readTextFile("posts.json")
 }
@@ -14,10 +20,50 @@ function readTextFile (file) {
 }
 
 function get_posts (x) {
- var posts = JSON.parse(x), i, a;
+ var posts = JSON.parse(x), i, a, b, c, trail = [], d;
  for (i = 0; posts[i]; i++) {
   a = document.createElement("div");
-  a.innerHTML = posts[i].item;
-  $("all_posts").appendChild(a)
+  b = a.cloneNode(false);
+  c = a.cloneNode(false);
+  a.innerHTML = people[posts[i].author - 1];
+  a.className = "author";
+  b.innerHTML = posts[i].body;
+  b.className = "itemBody";
+  c.id = "post_" + posts[i].id;
+  c.className = "post";
+  c.appendChild(a);
+  c.appendChild(b);
+  if (posts[i].replies_to) {
+   for (d = trail.length - 1; trail[d] != posts[i].replies_to && d >= 0; d--);
+   if (!$( "post_" + trail[d] ).nextSibling && d >= 0) {
+    $( "post_" + trail[d] ).parentNode.appendChild(c);
+    trail.push(posts[i].id);
+    continue
+   }
+  }
+  e = document.createElement("div");
+  e.id = "thread_" + $("all_posts").childNodes.length;
+  e.className = "thread";
+  e.appendChild(c);
+  $("all_posts").appendChild(e);
+  addEvent(e, "click", function () {
+   window.reply_to.unshift(this.lastChild.lastChild);
+   window.reply_to[0].style.backgroundColor = "gold";
+   window.reply_to[0].offsetHeight;
+   document.forms[0].reply_to.value = this.lastChild.id.split("_")[1]
+   setTimeout(function () {
+    window.reply_to[0].style.transition = "background-color 1s ease-in-out 1s";
+    window.reply_to[0].style.backgroundColor = null
+   }, 0);
+   setTimeout(function () {
+    var index = window.reply_to.length;
+    window.reply_to[index-1].style.transition = null;
+    window.reply_to.pop()
+   }, 2000)
+  })
+  trail.push(posts[i].id)
  }
 }
+
+var people = ["Aidan", "Cameron", "Nico", "Mel"],
+    reply_to = []
